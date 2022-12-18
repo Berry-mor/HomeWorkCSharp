4dd1e2b3f889459c04750e3cfa227db187a8559b
Eduard <pump31031966@gmail.com>
2022-12-18 21:33:09 +0300

    Task 4

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,6 +1,7 @@
-﻿/*
+﻿
 // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, 
 // а какое меньшее.
+/*
 int a = 0;
 int b = 0;
 Console.WriteLine("Введите первое число: ");
@@ -12,3 +13,42 @@ if (a > b) Console.WriteLine($"max = {a}");
     */
 
 // Напишите программу, которая принимает на вход три числа и выдает максимальное
+/*
+int a = 0;
+int b = 0;
+int c = 0;
+Console.WriteLine("Введите первое число: ");
+a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if (b > max) max = b;
+    if (c > max) max = c;
+Console.WriteLine();
+Console.WriteLine($" Максимальное число:  {max}");
+*/
+
+//Напишите программу, которая принимает на вход  число и выдает, является ли число четным
+
+/*int a = 0;
+Console.WriteLine("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+if ( a%2 == 0) Console.WriteLine("Да");
+else Console.WriteLine("Нет");
+*/
+
+// Напишите программу, которая принимает на вход  число (N), а выходе показывает все четные числа от 1 до N.
+/*int N = 0;
+Console.WriteLine("Введите число: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+for (int  i = 1; i <=  N; i++)
+{
+    if (i % 2 == 0) Console.Write($"{i},");
+}
+*/
+//КАК УБРАТЬ ПОСЛЕДНЮЮ ЗАПЯТУЮ В ОТВЕТЕ БЕЗ ПРЕОБРАЗОВАНИЯ.
+//КАК УБРАТЬ ПОСЛЕДНЮЮ ЗАПЯТУЮ В ОТВЕТЕ БЕЗ ПРЕОБРАЗОВАНИЯ.
