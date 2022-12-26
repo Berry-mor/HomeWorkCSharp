780a1cb3430f286a1ffd22a3c33b4ee5f54e34ea
Eduard <pump31031966@gmail.com>
2022-12-26 21:02:13 +0300

    Задание 3

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,67 +1,4 @@
 ﻿
-// Напишите программу, которая принимает
-// на вход  число и проверяет, является ли оно палиндромом.
-/*
-
-void Itog(int sum, int number)
-{
- //Console.WriteLine(sum);
- int ii = 10; 
-  int count = sum % 10;
-  int i = (sum - count) / ii;
-  int numLev = 0;
-  int numPr = 0;
-  for(;numLev == numPr && number > 9;)
-  {
-   numLev = number / i ;
-   numPr = number % 10 ;
- // Console.WriteLine($"Number {number},NumLev {numLev}, NumPr {numPr}");
-  number = (number % i) / 10;
-  //Console.WriteLine($"Number {number}");
-      if(number  > 9 && number < 100 )
-          {
-             numLev = number / i;
-             numPr =  number % 10;
-             //Console.WriteLine($"Number if {number},NumLev {numLev}, NumPr {numPr}");
-             if (numLev == numPr)
-                {
-                  Console.WriteLine($"ДА!!!");
-                  break;
-                }
-         }
-  ii = ii * 10;
-  i = i / ii;
-  }
-    
-       if (numLev == numPr && number < 9)
-      {
-          Console.WriteLine("ДА");
-      }
-    else
-      {
-         Console.WriteLine("НЕТ");
-       }
-}
- int  KolNumOne(int number)
-{
-    int i = 10;
-    int count = 1; // считает количество знаков числа
-    
-            for(;number > 10 && number / i != 0;)
-            {
-                i = i * 10;
-               count++;
-               }
-            return  i + count;
-        
-}
-
-Console.WriteLine("Введите число:");
-int num = Convert.ToInt32(Console.ReadLine());
-int sum = KolNumOne(num);
-Itog(sum, num);
-*/
- 
 
 //Задача № 21
 //Напишите программу, которая принимает на вход координаты двух
@@ -94,7 +31,7 @@ void dist(float x1, float y1,
     return;
 }
 */
-
+/*
 //Задача №23
 //Напишите программу, которая принимает
 // на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
@@ -112,8 +49,9 @@ void CubNum(int number)
 }
 
 
-/*
-//Второй вариант задачи 19
+*/
+//Задача № 19
+//Напишите программу, которая принимает на вход  число и проверяет, является ли оно палиндромом.
 
 int getCharLength(int number)
 {
@@ -193,4 +131,3 @@ Console.WriteLine($"Right : {right}");
     }
 
     return 0;
-*/
