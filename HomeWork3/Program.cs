483afca0deaf32e9b5663232f91a0ae235943141
Eduard <pump31031966@gmail.com>
2022-12-26 15:33:29 +0300

    Task 21

--- HomeWork3/Program.cs
+++ HomeWork3/Program.cs
@@ -1,9 +1,10 @@
-﻿// Напишите программу, которая принимает
+﻿
+// Напишите программу, которая принимает
 // на вход  число и проверяет, является ли оно палиндромом.
-
+/*
 Console.WriteLine("Введите число:");
 int num = Convert.ToInt32(Console.ReadLine());
-KolNum(num);
+//KolNum(num);
 int sum = KolNumOne(num);
 Itog(sum, num);
 void Itog(int sum, int number)
@@ -21,7 +22,7 @@ void Itog(int sum, int number)
   Console.WriteLine($"Number {number},NumLev {numLev}, NumPr {numPr}");
   number = (number % i) / 10;
   Console.WriteLine($"Number {number}");
-      /*if(number  > 9 && number < 100 )
+      if(number  > 9 && number < 100 )
           {
              numLev = number / i;
              numPr =  number % 10;
@@ -36,18 +37,11 @@ void Itog(int sum, int number)
          {
             ii = ii * 10;
             i = i / ii;
-         }*/
+         }
   ii = ii * 10;
   i = i / ii;
   }
-    /*if (numLev == numPr && number < 9)
-      {
-          Console.WriteLine("ДА");
-      }
-    else
-      {
-         Console.WriteLine("НЕТ");
-       }*/
+    
        if (numLev == numPr && number < 9)
       {
           Console.WriteLine("ДА");
@@ -76,4 +70,35 @@ void KolNum(int number)
    
      if(number < 10) Console.WriteLine("Число однозначное");   
 }
- 
+ */
+
+//Задача № 21
+//Напишите программу, которая принимает на вход координаты двух
+// точек и находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите x1:");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y1:");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z1:");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите x2:");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите y2:");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите z2:");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+ distance(x1, y1, z1,
+             x2, y2, z2);
+void distance(float x1, float y1,
+              float z1, float x2,
+              float y2, float z2)
+{
+    double d = Math.Pow((Math.Pow(x2 - x1, 2) +
+                         Math.Pow(y2 - y1, 2) +
+                         Math.Pow(z2 - z1, 2) *
+                                   1.0), 0.5);
+    Console.WriteLine(Math.Round(d,2));
+    return;
+}
